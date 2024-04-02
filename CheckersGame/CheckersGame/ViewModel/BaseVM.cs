e3b868b563f41aba9e25e13c3df1d99b8dd3aea5
Marian <80158909+SUGAARxD@users.noreply.github.com>
2024-04-02 11:48:26 +0300

    Added commands for the buttons of MenuWindow

---
+++ CheckersGame/CheckersGame/ViewModel/BaseVM.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel;
+
+namespace CheckersGame.ViewModel
+{
+    public class BaseVM : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public void NotifyPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+    }
+}
--- CheckersGame/CheckersGame/ViewModel/MenuVM.cs
+++ CheckersGame/CheckersGame/ViewModel/MenuVM.cs
@@ -1,7 +1,85 @@
 ﻿
+
+using System.Windows.Input;
+
 namespace CheckersGame.ViewModel
 {
-    internal class MenuVM
+    internal class MenuVM : BaseVM
     {
+        public MenuVM()
+        {
+
+        }
+
+        #region Commands
+
+        private ICommand _newGameCommand;
+        public ICommand NewGameCommand
+        {
+            get
+            {
+                if (_newGameCommand == null)
+                    _newGameCommand = new RelayCommand(null);
+                return _newGameCommand;
+            }
+        }
+
+        private ICommand _loadGameCommand;
+        public ICommand LoadGameCommand
+        {
+            get
+            {
+                if (_loadGameCommand == null)
+                    _loadGameCommand = new RelayCommand(null);
+                return _loadGameCommand;
+            }
+        }
+
+        private ICommand _customGameCommand;
+        public ICommand CustomGameCommand
+        {
+            get
+            {
+                if (_customGameCommand == null)
+                    _customGameCommand = new RelayCommand(null);
+                return _customGameCommand;
+            }
+        }
+
+        private ICommand _howToPlayCommand;
+        public ICommand HowToPlayCommand
+        {
+            get
+            {
+                if (_howToPlayCommand == null)
+                    _howToPlayCommand = new RelayCommand(null);
+                return _howToPlayCommand;
+            }
+        }
+
+        private ICommand _settingsCommand;
+        public ICommand SettingsCommand
+        {
+            get
+            {
+                if (_settingsCommand == null)
+                    _settingsCommand = new RelayCommand(null);
+                return _settingsCommand;
+            }
+        }
+
+        private ICommand _creditsCommand;
+        public ICommand CreditsCommand
+        {
+            get
+            {
+                if (_creditsCommand == null)
+                    _creditsCommand = new RelayCommand(null);
+                return _creditsCommand;
+            }
+        }
+
+        #endregion
+
     }
 }
